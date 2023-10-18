738b610249bb98913aec12647cb23028e3dd6716
fengtao92 <1440913385@qq.com>
2023-10-18 15:48:25 +0800

    commit

--- Platform/bilibili_livedanmaku.py
+++ Platform/bilibili_livedanmaku.py
@@ -2,7 +2,7 @@
 Author: wolffy
 Date: 2023-10-18 14:02:35
 LastEditors: fengtao92 1440913385@qq.com
-LastEditTime: 2023-10-18 15:38:46
+LastEditTime: 2023-10-18 15:46:09
 FilePath: /EWVtuber/Platform/bilibili_livedanmaku.py
 Description: 项目名称：虚拟主播软件
 版权所有：北京光线传媒股份有限公司
@@ -17,6 +17,7 @@ from bilibili_api.login import login_with_password, login_with_sms, send_sms, Ph
 from bilibili_api.user import get_self_info
 from bilibili_api import settings
 from bilibili_api import sync
+from bilibili_api import login, user, sync
 
 class bilibiliDanmaku(object):
 
@@ -24,61 +25,72 @@ class bilibiliDanmaku(object):
     def __init__(self) -> None:
         pass
 
-    def login():
-        mode = int(input("""请选择登录方式：
-        1. 密码登录
-        2. 验证码登录
-        请输入 1/2
-        """))
-
-        credential = None
-
-        # 关闭自动打开 geetest 验证窗口
-        settings.geetest_auto_open = False
-
-        if mode == 1:
-            # 密码登录
-            username = input("请输入手机号/邮箱：")
-            password = input("请输入密码：")
-            print("正在登录。")
-            c = login_with_password(username, password)
-            if isinstance(c, Check):
-                # 还需验证
-                phone = print("需要进行验证。请考虑使用二维码登录")
-                exit(1)
-            else:
-                credential = c
-            print("登录成功")
-        elif mode == 2:
-            # 验证码登录
-            phone = input("请输入手机号：")
-            print("正在登录。")
-            send_sms(PhoneNumber(phone, country="+86")) # 默认设置地区为中国大陆
-            code = input("请输入验证码：")
-            c = login_with_sms(PhoneNumber(phone, country="+86"), code)
-            if isinstance(c, Check):
-                # 还需验证
-                phone = print("需要进行验证。请考虑使用二维码登录")
-                exit(1)
-            else:
-                credential = c
-            print("登录成功")
-        else:
-            print("请输入 1/2 ！")
+    def login(self):
+        print("请登录：")
+        credential = login.login_with_qrcode_term() # 在终端扫描二维码登录
+        # credential = login.login_with_qrcode() # 使用窗口显示二维码登录
+        try:
+            credential.raise_for_no_bili_jct() # 判断是否成功
+            credential.raise_for_no_sessdata() # 判断是否成功
+        except:
+            print("登陆失败。。。")
             exit()
+        print("欢迎，", sync(user.get_self_info(credential))['name'], "!")
+        # mode = int(input("""请选择登录方式：
+        # 1. 密码登录
+        # 2. 验证码登录
+        # 请输入 1/2
+        # """))
 
-        if credential != None:
-            name = sync(get_self_info(credential))['name']
-            print(f"欢迎，{name}!")
+        # credential = None
+
+        # # 关闭自动打开 geetest 验证窗口
+        # settings.geetest_auto_open = False
+
+        # if mode == 1:
+        #     # 密码登录
+        #     username = input("请输入手机号/邮箱：")
+        #     password = input("请输入密码：")
+        #     print("正在登录。")
+        #     c = login_with_password(username, password)
+        #     if isinstance(c, Check):
+        #         # 还需验证
+        #         phone = print("需要进行验证。请考虑使用二维码登录")
+        #         exit(1)
+        #     else:
+        #         credential = c
+        #     print("登录成功")
+        # elif mode == 2:
+        #     # 验证码登录
+        #     phone = input("请输入手机号：")
+        #     print("正在登录。")
+        #     send_sms(PhoneNumber(phone, country="+86")) # 默认设置地区为中国大陆
+        #     code = input("请输入验证码：")
+        #     c = login_with_sms(PhoneNumber(phone, country="+86"), code)
+        #     if isinstance(c, Check):
+        #         # 还需验证
+        #         phone = print("需要进行验证。请考虑使用二维码登录")
+        #         exit(1)
+        #     else:
+        #         credential = c
+        #     print("登录成功")
+        # else:
+        #     print("请输入 1/2 ！")
+        #     exit()
+
+        # if credential != None:
+        #     name = sync(get_self_info(credential))['name']
+        #     print(f"欢迎，{name}!")
 
     def startServer(self):
+        credential = self.login()
         # 自己直播间号
         ROOMID = 30923980
         # 凭证 根据回复弹幕的账号填写
-        credential = Credential(
-            sessdata="",
-            bili_jct=""
-        )
+        # credential = Credential(
+        #     sessdata="",
+        #     bili_jct=""
+        # )
         # 监听直播间弹幕
         monitor = LiveDanmaku(ROOMID, credential=credential)
         # 用来发送弹幕
