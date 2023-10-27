f931a1c038b0f2174c8445057c2de97a83fd326b
fengtao92 <1440913385@qq.com>
2023-10-27 14:57:19 +0800

    commit

---
+++ API/vtuber_api.py
@@ -0,0 +1,13 @@
+"""
+EWVtuber
+项目名称：虚拟主播软件
+版权所有：北京光线传媒股份有限公司
+技术支持：北京光线传媒股份有限公司
+Editor:fengtao
+Mails:fengtao23@mails.ucas.ac.cn
+"""
+
+
+def langchain_host() -> str:
+    host = 'http://172.23.0.191:7866'
+    return host
---
+++ vtuber.py
@@ -0,0 +1,49 @@
+"""
+EWVtuber
+项目名称：虚拟主播软件
+版权所有：北京光线传媒股份有限公司
+技术支持：北京光线传媒股份有限公司
+Editor:fengtao
+Mails:fengtao23@mails.ucas.ac.cn
+"""
+
+from command import CommandManager
+from Audio.player import AudioPlayer
+from EWSpeech.speech import Speaker
+
+
+class Vtuber(object):
+    _instance = None
+    cmd_manager: CommandManager = None  # 命令行控制器
+    audio_player: AudioPlayer = None  # 音频播放器
+    speaker: Speaker = None
+
+    # 单例模式
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Vtuber, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self):
+        self.cmd_manager = CommandManager()
+        self.audio_player = AudioPlayer()
+        self.cmd_manager.audio_manager = self.audio_player
+
+    # 启动命令行界面模式
+    def start_command_line(self):
+        while True:
+            cmd = input('>> ')
+            is_break = self.cmd_manager.check_cmd(cmd=str(cmd))
+            if cmd == '--aaa':
+                self.audio_player.test()
+            if is_break:
+                break
+
+
+def audio_player() -> AudioPlayer:
+    player = Vtuber().audio_player
+    return player
+# default_vtuber = Vtuber()
+# cmd_manager = default_vtuber.cmd_manager
+# audio_player = default_vtuber.audio_player
+# speaker = default_vtuber.speaker
